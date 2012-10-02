7b33a9d69c35ceb33cd737bd8a1f491a399365a3
Wise Horo <that4chanwolf@gmail.com>
2012-10-02 06:48:57 -0400

    MAL description grabbing

---
+++ assets/js/info.js
@@ -0,0 +1,31 @@
+var get = function(name) {
+	var xhr = new XMLHttpRequest(),
+	    res;
+	name = escape(name.replace(/ /g, '+').toLowerCase());
+	xhr.open("GET", "http://mal-api.com/anime/search?q=" + name, false);
+	xhr.send();
+	if(xhr.status === 200) {
+		res = JSON.parse(xhr.responseText);
+	}
+	return res;
+}
+
+var set = function(info) {
+	var des;
+	if(typeof info === 'undefined' && info === null) {
+		console.error(new Error("Info not supplied to set()"));
+		return;
+	}
+	for(var i in info) {
+		if( info[i].title === $('div.hero-unit h1')[0].textContent ) {
+			des = info[i].synopsis;
+			id = info[i].id;
+			break;
+		}
+	}
+	$('div.hero-unit p.description')[0].innerHTML = des + '  <a href="http://myanimelist.net/anime.php?id='+ id + '">[MORE]</a>' || "";
+}
+
+$(document).ready(function() {
+	set(get($('div.hero-unit h1')[0].textContent));
+});
